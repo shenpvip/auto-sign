a42ac5ec23c0dd8f007485076a16a5ecd9b71239
shenpeng <396635614@qq.com>
2024-11-05 17:01:40 +0800

    fix：update

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const data = process.env.ACCOUNTS_JSON
-console.log(data,‘data------’)
-console.log(data.serv00,‘serv00------’)
-console.log(data.serv00.userName,‘userName------’)
+console.log(data, "data------")
+console.log(data.serv00, "serv00------")
+console.log(data.serv00.userName, "userName------")
