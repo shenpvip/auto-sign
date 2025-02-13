175d4321919d23583bb165ee6256b3cb59086d6f
Roc° <396635614@qq.com>
2025-02-13 17:47:52 +0800

    Update asyncFile.js

--- script/asyncFile.js
+++ script/asyncFile.js
@@ -72,7 +72,7 @@ async function downloadMatchingFilesRecursive(repoOwner, repoName, pattern) {
           // 解压 ZIP 文件
           extractZipToDir(
             fileName,
-            path.join(__dirname, `../public/${repoName}`)
+            path.join(__dirname, `${repoName}`)
           )
 
           // 可选：删除 ZIP 文件
